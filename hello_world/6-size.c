3678c9886f9ea4261332703b44a31598c191c1f9
moezabdelkefi <moezabdelkefi17@gmail.com>
2022-09-30 07:26:25 -0700

    C program that prints the size of various types on the computer it is compiled and run on

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,4 +14,4 @@ printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("Size of double: %zu bytes\n", sizeof(doubleType));
 printf("Size of char: %zu byte\n", sizeof(charType));
-return 0;
+return (0);
