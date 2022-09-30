b81df89706db127bdde330a352d60e6fa4e75d37
moezabdelkefi <moezabdelkefi17@gmail.com>
2022-09-30 05:28:49 -0700

    a C program that prints exactly "Programming is like building a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	//string initialisation
+	
     char Mystr[] = "\"Programming is like building a multilingual puzzle";
     
-    puts(Mystr); //writing the string to stdout
+    puts(Mystr); 
     
     return 0;
 }
