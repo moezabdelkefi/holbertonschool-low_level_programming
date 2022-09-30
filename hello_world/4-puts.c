4f698e92334e110fecf94fbc8f11a5703e22b320
moezabdelkefi <moezabdelkefi17@gmail.com>
2022-09-30 05:54:40 -0700

    delete the new line caractere

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 int main()
 {
 	
-    char Mystr[] ="\"Programming is like building a multilingual puzzle\n";
+    char Mystr[] = "\"Programming is like building a multilingual puzzle";
     
     puts(Mystr);
     return 0;
