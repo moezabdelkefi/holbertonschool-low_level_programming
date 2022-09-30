0a60b55a55a8b006e339eb3d40f575eb509cec9a
moezabdelkefi <moezabdelkefi17@gmail.com>
2022-09-30 08:26:20 -0700

    Write a C program that prints exactly

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-int main(void){
-("and that piece of art is useful" "- Dora Korpar, 2015-10-19");
+{#include <stdio.h>
+int main(void) {
+write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
 return (1);
-}
+} }
