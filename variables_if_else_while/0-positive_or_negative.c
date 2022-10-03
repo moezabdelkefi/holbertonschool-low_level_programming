792982803b5a165fe520adf89bdd8f9e6ff6a347
moezabdelkefi <moezabdelkefi17@gmail.com>
2022-10-03 02:24:46 -0700

    This program will assign a random number to the variable n each time it is executed

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ( n > 0 )
-    System.out.printf ( " % d is a Positive Number " , number ) ;
+	printf("Min : 1 Max : 40 %d\n", random_number(1,40));
+        printf("Min : 100 Max : 1000 %d\n",random_number(100,1000));
 	return (0);
 }
