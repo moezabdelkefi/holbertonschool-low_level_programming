4d6434e24ad15e728527f6060a22ec469e6d7e81
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-03 05:06:43 -0930

    Write a program that prints the alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - print random number to variable
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char ch = 'a';
+    for (ch = 'a'; ch <= 'z'; ch++)
+        putchar(ch);
+  
+    return (0);
+}
