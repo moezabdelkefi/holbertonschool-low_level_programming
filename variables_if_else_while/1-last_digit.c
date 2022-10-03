39710df2c49905ad8edd49bdcc82d09f55b84fae
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-03 04:17:56 -0930

    This program will assign a random number to the variable n each time it is executed

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -7,16 +7,16 @@
  * Return: Always 0 (Success)
  */
 {
-int n ;
-char last [ ] = "Last digit of";
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf ("%s %d is %d and is" ,last, n, n % 10);
+    int n ;
+    char last [] = "Last digit of";
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    printf ("%s %d is %d and is" ,last, n, n % 10);
 if (n % 10 > 5)
 {
     printf ("greater than 5\n");
 }
-else if ( n % 10 == 0 )
+else if ( n % 10 == 0)
 {
     printf ("0\n");
 }
