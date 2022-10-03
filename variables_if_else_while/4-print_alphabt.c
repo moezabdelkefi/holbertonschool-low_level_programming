23d5e376693531ced1e9a924b75363ef36ba4f45
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-03 06:57:12 -0930

    a program that prints the alphabet in lowercase

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+
+	for (ch= 'q'; ch<= 'e'; ch++)
+
+	putchar(ch);
+
+	putchar ('\n') ;
+
+}
