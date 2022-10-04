78d4630281375878ac800cfba032c3661caa2866
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-03 22:25:11 -0930

    Write a program that prints all single digit numbers of base 10 starting from 0

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main -a program that prints the alphabet in lowercase
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
