62b6a81b882e010f9bfdb15af290329d87d86421
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-04 00:04:25 -0930

    a program that prints all the numbers of base 16 in lowercase

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *main -a program that prints the alphabet in lowercase
+ *main -a rogram that prints all the numbers of base 16 in lowercase
  *Return: Always 0 (Success)
  */
 int main(void)
