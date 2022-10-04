c52bab4be58e432136cb085a7dbcb291f907fa0c
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-03 23:41:35 -0930

    a program that prints all the numbers of base 16 in lowercas

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; num < 16; num++)
+	for (num = 0; num < 9; num++)
 		putchar((num % 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
