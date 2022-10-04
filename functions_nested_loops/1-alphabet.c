460fa94ebc548f6aa4576ce4612e37fb3c6a811a
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-04 04:50:06 -0930

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * print_alphabet - check the code
- * 
+ *
  */
 void print_alphabet(void)
 {
-    int c;
+	int c;
 
-    for (c = 97; c <= 122 ; c++)
-   		 _putchar(c);
-    _putchar('\n');
+	for(c = 97; c <= 122; c++)
+		_putchar(c);
+	_putchar('\n');
 }
-
