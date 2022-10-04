ec1f5852d77e89fc44aa1951405a55e2ec88d62c
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-03 23:58:20 -0930

    a program that prints all the numbers of base 16 in lowercase

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -11,8 +11,7 @@ int main(void)
 	char letter;
 
 	for (num = 0; num < 9; num++)
-	putcher(letter);
-		putchar((num % 10) + '0');
+		putchar((num % 9 + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
