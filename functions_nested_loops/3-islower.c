222d621c0b8f7ca8670c90be9da6f698bbeeb2c6
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-04 22:22:23 -0930

    function that checks for lowercase character

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -6,20 +6,10 @@
  */
 int main(void)
 {
-int r;
+int _islower(int c)
 {
-	if (r >= 'a' && r <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
-}
-
-    r =_islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
 }
