571b1d98a82fe6914e296ac234ec48d1dad640f8
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-04 20:32:29 -0930

    print_x_10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@ void print_alphabet_x10(void)
 
 	counter = 0;
 
-	while (co < 10)
+	while (counter < 10)
 	{
 		for (n = 'a'; n <= 'z'; n++)
 		{
 			_putchar(n);
 		}
-		co++;
+		counter++;
 		_putchar('\n');
 	}
 }
