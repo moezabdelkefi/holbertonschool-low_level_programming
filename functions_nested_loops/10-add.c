dcf52660e6bcd2f673324f4a1a74f77eddfef546
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-06 00:23:48 -0930

    function that adds two integers and returns the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  add - function that adds two integers and returns the result
+ *@a: first paramet
+ *@b: second paramet
+ * Return: Always 0.
+ */
+int add(int number1, int number2)
+{
+	return (number1 + number2);
+}
