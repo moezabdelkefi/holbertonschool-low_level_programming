bb45462c79742751a254687fee4a517c704950bd
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-07 05:57:25 -0930

    function that prints the numbers, from 0 to 9, followed by a new line

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int i;
+
+	for (i = 0; i < 10; i++)
+{
+	putchar(i + '0');
+}
+	putchar('\n');
+}
