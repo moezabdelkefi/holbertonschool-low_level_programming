bae323f555418100daef18da938ed0377d9a154b
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-07 05:24:03 -0930

    a function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul- function that multiplies two integers
+ *@a: integer to use
+ *@b: integer to use
+ * Return: A*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
