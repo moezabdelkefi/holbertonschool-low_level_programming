788524bd8ab784d03bd9809209e9ae88398912e1
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-10 04:46:29 -0930

    a function that prints a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ * print_rev -print a string
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+
+	{
+	int l = 0, i = 0;
+
+	while (s[i++])
+		l++;
+
+	for (i = l - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
+}
