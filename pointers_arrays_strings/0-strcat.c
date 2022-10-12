918d6c6fd3e67a33ca47b39d222621afcf87190b
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-12 06:27:55 -0930

    a function that concatenates two strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,9 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int l, s;
 
- 	l = strlen(src);
-	s = strlen(dest);
-	for (i = '0'; i <= l; i++)
+l = strlen(src);
+s = strlen(dest);
+	for (i = 0; i <= l; i++)
 	{
 		dest[s + i] = src[i];
 	}
