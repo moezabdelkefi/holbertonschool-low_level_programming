0836e34774c57c6c6359233ccc1588fb7b23a2a6
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-14 07:41:22 -0930

    function that copies memory area

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,5 +21,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
