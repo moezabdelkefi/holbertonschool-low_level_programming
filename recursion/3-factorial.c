ed3d46c5f6129a2ed4aab96a193fc5e0347d3508
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-17 03:35:43 -0930

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 1-main.c 1-print_rev_recursion.c -o 1-print_rev_recursion

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * factorial - returns the factorial of a given number
+ *@n: input value
+ * Return: -1
  */
 int factorial(int n)
 {
 	if (n == 0)
-		return 1;
-	else if(n<0)
-		return -1;
+		return (1);
+	else if (n < 0)
+		return (-1);
 	else
-		return n * factorial(n - 1);
+		return (n * factorial(n - 1));
 }
