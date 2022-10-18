a16fddbe6a84f6a00a70ba4b2e79269fd4437552
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-18 02:37:18 -0930

    function that returns 1 if the input integer is a prime number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,11 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * check_prime_number - function that returns 1
+ * @x: input varible
+ * @y: input variable
+ * Return: 1 or 0
+ */
+int check_prime_number(int x, int y)
+{
+	if (x == 1)
+		return (0);
+	if (y == x)
+		return (1);
+	if (y > x)
+		return (0);
+	if (x % y == 0)
+		return (0);
+	return (check_prime_number(x, y + 1));
+}
+/**
+ * is_prime_number - function that returns 1
+ * @n: input
+ * Return: 1 or 0
  */
 int is_prime_number(int n)
 {
-	
+	return (check_prime_number(n, 2));
 }
