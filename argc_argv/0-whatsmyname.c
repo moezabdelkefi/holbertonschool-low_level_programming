47bae0ee660a9ac9d9114b2e4c6400c0617f2f88
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-19 00:28:47 -0930

    a program that prints its name, followed by a new line

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+/**
+ * main - print name
+ *@argv:input
+ *@argc:input
+ * Return:  0.
+ */
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s ", argv[i]);
