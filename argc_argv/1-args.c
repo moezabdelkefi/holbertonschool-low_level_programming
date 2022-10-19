60c7e22cc3d85efd5c185beaf02c5112ad58d485
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-19 01:14:34 -0930

    print number

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print number
+ *@argv:input
+ *@argc:input
+ * Return:  0.
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+
+	printf("%d\n", argc);
+
+	return (0);
+}
