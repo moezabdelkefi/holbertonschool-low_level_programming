a8604779baa670c2aa48103dcb100b6e82c2a5de
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-25 04:08:09 -0930

    a function that concatenates two strings

--- more_malloc_free/1-main.c
+++ more_malloc_free/1-main.c
@@ -9,9 +9,13 @@
 int main(void)
 {
     char *concat;
+	char *cn;
 
     concat = string_nconcat("Best ", "School !!!", 6);
+	cn = string_nconcat("tik", "Hello", 12);
     printf("%s\n", concat);
+	printf("%s\n", cn);
     free(concat);
+	free(cn);
     return (0);
 }
--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,11 +12,25 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *array;
-	char *buffer = malloc(n * sizeof(char));
+	char *buffer;
+	if (n >= strlen(s2)) {
+		n = strlen(s2);
+	}
+	buffer = malloc(n * sizeof(char));
+
+	if(s1 == NULL){
+		s1 = "";
+	}
+	if(s2 == NULL){
+		s2 = "";
+	}
 
 	strncpy(buffer, s2, n);
 	array = malloc((strlen(s1) + strlen(buffer) + 1) * sizeof(char));
 	strcat(array, s1);
 	strcat(array, buffer);
+	if(array == NULL) {
+		return NULL;
+	}
 	return (array);
 }
