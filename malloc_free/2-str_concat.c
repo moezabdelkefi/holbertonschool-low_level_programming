ed607f842731e057416c11ee3d213344804d8855
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-27 03:00:43 -0930

    a function that concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,9 +11,19 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *array3;
-
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	array3 = malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
-
+	if (array3 == NULL)
+	{
+		return (NULL);
+	}
 	strcat(array3, s1);
 	strcat(array3, s2);
 
