3e04d9076b289899537ae8d5925cd255a454288c
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-30 22:56:41 -0930

    Define a new type struct dog with the following elements

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - Define a new type struct dog
+ *@name:input
+ *@age: input
+ *@owner:input
+ * Return: Always 0.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+}  struct dog
+#endif
