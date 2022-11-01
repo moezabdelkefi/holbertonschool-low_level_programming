9d4b46b8fc4e98e0a39c94f16a5f658c992693ba
root <root@DESKTOP-C0MK0L2.localdomain>
2022-10-31 22:54:52 -0930

    a function that prints a struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,19 +2,13 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * iprint_dog -  a function that prints a struct dog
+ * print_dog -  a function that prints a struct dog
  *@d:input
  * Return: Always 0.typedef struct
  */
 void print_dog(struct dog *d)
 {
-	if (d = NULL)
-	{
-		print(nil);
-	}
-	if (name = NULL)
-	{
-		print(Name: (nil));
-	}
-	if (d = NULL)
+	d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+	printf("Age: %f\n", d->age);
+	d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
 }
