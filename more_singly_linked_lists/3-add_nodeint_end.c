cf8794b19a9ee4f642960304265e758a2a923b83
root <root@DESKTOP-C0MK0L2.localdomain>
2022-11-17 05:25:54 -0930

    add a new node a the end of the list

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ *add_nodeint_end - add a new node at the end of the liste
+ *@head: argument
+ *@n:argument
+ *Return: the new node
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newNode = malloc(sizeof(listint_t));
+
+	newNode->n = n;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+		*head = newNode;
+
+	else
+	{
+		listint_t *lastNode = *head;
+
+		while (lastNode->next != NULL)
+		{
+			lastNode = lastNode->next;
+		}
+
+		lastNode->next = newNode;
+	}
+	return (newNode);
+}
