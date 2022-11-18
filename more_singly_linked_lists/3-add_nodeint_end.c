da1b9337e783c3fcc050e44a580116e59fb4f27e
root <root@DESKTOP-C0MK0L2.localdomain>
2022-11-18 01:18:02 -0930

    add a new node at the end of the list

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -24,8 +24,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			lastNode = lastNode->next;
 		}
 
-		free(*head);
 		lastNode->next = newNode;
+		free(newNode);
 	}
 	return (newNode);
 }
