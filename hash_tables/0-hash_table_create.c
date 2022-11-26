4120fc702d31b764ac1b2545e462edd57529ccac
root <root@DESKTOP-C0MK0L2.localdomain>
2022-11-26 10:29:42 -0930

    create a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned int i = 0;
+	unsigned long int i;
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
