9eaab700b96707d08926a47b4561bdc5ee6ab1df
root <root@DESKTOP-C0MK0L2.localdomain>
2022-11-26 10:28:11 -0930

    create a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,14 +9,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned int i;
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
+	if (table == NULL)
+		return (NULL);
 	table->size = size;
-	table->size = 0;
+	table->array = malloc(sizeof(hash_table_t) * size);
+	if (table->array == NULL)
+		return (NULL);
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
-	if (table == NULL)
-	{
-		free(table);
-		return (NULL);
-	}
 	return (table);
 }
