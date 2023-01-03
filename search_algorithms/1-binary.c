2315e5a13df3aa19a963e417d7e524c791d930d5
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-03 04:04:20 -0930

    adding a function

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+/**
+ * binary_search - a function that searches for a value in a sorted array
+ * @array: is pointer to the first element
+ * @size: number of the element
+ * @value: value to search for
+ * Return: (-1)
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t mid, i;
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		printf("searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+
+			if (i < high)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+	}
+	return (-1);
+}
