2f5e8143917e0b7056dc222762922b24fae4dbcf
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-03 02:45:12 -0930

    fix the file

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,6 +1,10 @@
 #include "search_algos.h"
 /**
- *
+ *linear_search - a function that searches for a value in an array of integers
+ * @array: is pointer to the first element
+ * @size: number of the element
+ * @value: value to search for
+ * Return: 
  */
 int linear_search(int *array, size_t size, int value)
 {
@@ -8,17 +12,17 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 
 	for (i = 0; i < size; i++)
 	{
-		printf("value checked array [%d] = [%d]\n ", array[i], value);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			return i;
+			return (i);
 		}
 	}
 
-	return -1;
+	return (-1);
 }
