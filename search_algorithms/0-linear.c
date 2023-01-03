96bb0c1d21b167a93d08610ce6bbcabf090c9596
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-03 02:19:00 -0930

    add a function

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+ *
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return -1;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		printf("value checked array [%d]\n ", array[i]);
+		if (array[i] == value)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
